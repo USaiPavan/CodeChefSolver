e4ea62da236fd1b95b6e42d5d2f86aae21c30b35
Sai Pavan Viswanath Upadhyayula <usaipavan@outlook.com>
2016-09-06 10:50:06 -0500

    Adding few solutions from HackerEarth code challenge

---
+++ HackerEarthSolver/Solutions/TheNormalType_09012016.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerEarthSolver.Solutions
+{
+    public static class TheNormalType_09012016
+    {
+        public static void Execute()
+        {
+            var totalInputCount = Console.ReadLine();
+            var inputArray = Console.ReadLine();
+
+            RunProgram(totalInputCount, inputArray);
+        }
+
+        public static void RunProgram(string totalInputCount, string inputValues)
+        {
+            var inputArray = inputValues.Split(' ');
+            var input = new Dictionary<int, int>();
+            var currentIndex = 0;
+            foreach (var element in inputArray)
+            {
+                var requiredNumber = 0;
+                for (var i = 0; i < element.Length; i++)
+                    requiredNumber = requiredNumber * 10 + (element[i] - '0');
+                input.Add(currentIndex, requiredNumber);
+                currentIndex++;
+            }
+            
+            var fullWindow = CountDistinct(input, input.Count);
+            var fullWindowDistinctCount = fullWindow[0];
+            var totalWindowMatch = 0;
+
+            for (var index = 2; index <= input.Count - 1; index++)
+            {
+                totalWindowMatch += CountDistinct(input, index, fullWindowDistinctCount);
+            }
+
+            totalWindowMatch++;
+            Console.WriteLine(totalWindowMatch);
+        }
+
+        static List<int> CountDistinct(Dictionary<int, int> inputArray, int windowSize)
+        {
+            var window = new Dictionary<int, int>();
+            var returnSet = new List<int>();
+            var distinctCount = 0;
+
+            for (var index = 0; index < windowSize; index++)
+            {
+                if (!window.ContainsKey(inputArray[index]))
+                {
+                    window.Add(inputArray[index], 1);
+                    distinctCount++;
+                }
+                else
+                {
+                    window[inputArray[index]]++;
+                }
+            }
+
+            returnSet.Add(distinctCount);
+
+            for (var index = windowSize; index < inputArray.Count; index++)
+            {
+                var firstValue = inputArray[index - windowSize];
+                if (window[firstValue] == 1)
+                {
+                    window.Remove(firstValue);
+                    distinctCount--;
+                }
+                else
+                {
+                    window[firstValue]--;
+                }
+
+
+                var currentValue = inputArray[index];
+                if (!window.ContainsKey(currentValue))
+                {
+                    window.Add(inputArray[index], 1);
+                    distinctCount++;
+                }
+                else
+                {
+                    window[currentValue]++;
+                }
+                returnSet.Add(distinctCount);
+            }
+
+            return returnSet;
+        }
+
+
+
+        static int CountDistinct(Dictionary<int, int> inputArray, int windowSize, int compareValue)
+        {
+            var window = new Dictionary<int, int>();
+            var returnValue = 0;
+            var distinctCount = 0;
+
+            for (var index = 0; index < windowSize; index++)
+            {
+                if (!window.ContainsKey(inputArray[index]))
+                {
+                    window.Add(inputArray[index], 1);
+                    distinctCount++;
+                }
+                else
+                {
+                    window[inputArray[index]]++;
+                }
+            }
+
+            if (distinctCount == compareValue)
+            {
+                returnValue++;
+            }
+
+            for (var index = windowSize; index < inputArray.Count; index++)
+            {
+                var firstValue = inputArray[index - windowSize];
+                if (window[firstValue] == 1)
+                {
+                    window.Remove(firstValue);
+                    distinctCount--;
+                }
+                else
+                {
+                    window[firstValue]--;
+                }
+
+                var currentValue = inputArray[index];
+                if (!window.ContainsKey(currentValue))
+                {
+                    window.Add(currentValue, 1);
+                    distinctCount++;
+                }
+                else
+                {
+                    window[currentValue]++;
+                }
+                if (distinctCount == compareValue)
+                {
+                    returnValue++;
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ HackerEarthSolver/Solutions/ThePsychicType_09012016.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerEarthSolver.Solutions
+{
+    public static class ThePsychicType_09012016
+    {
+        static int[] _input;
+        static HashSet<int> _previousPositions = new HashSet<int>();
+
+        public static void JudgeEntry()
+        {
+            var totalNumbers = Console.ReadLine();
+            var inputArray = Console.ReadLine().Trim().Split(' ');
+            var teleportationInfo = Console.ReadLine().Trim().Split(' ');
+            _input = Array.ConvertAll(inputArray, int.Parse);
+            var result = SolveChallenge(teleportationInfo);
+            Console.WriteLine(result);
+        }
+
+
+        public static string SolveChallenge(string[] teleportationInfo)
+        {
+            return TraverseArray(int.Parse(teleportationInfo[0]), int.Parse(teleportationInfo[1])) ? "Yes" : "No";
+        }
+
+        static bool TraverseArray(int currentPosition, int requiredPosition)
+        {
+            if (currentPosition == requiredPosition)
+            {
+                return true;
+            }
+
+            if (_previousPositions.Contains(currentPosition))
+            {
+                return false;
+            }
+
+            if (currentPosition < 1)
+            {
+                return false;
+            }
+
+            _previousPositions.Add(currentPosition);
+            return TraverseArray(_input[currentPosition - 1], requiredPosition);
+        }
+    }
+}
